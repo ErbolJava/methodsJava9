d24a528c2088088be585e385d7c58b7009ceb169
user <protanki002@gmail.com>
2023-01-26 16:04:08 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(method(new String[]{"car","emotion","smile"}));
+        }
+public static String method(String [] word) {
+    Scanner scanner = new Scanner(System.in);
+    String a = scanner.nextLine();
+    if (word.equals(scanner)) {
+        return "Siz izdegen soz massivde bar";
+    } else {
+        return "Siz izdegen soz massivde bar";
+    }
+}
+}
+
